d4e98913cb5ce1479d0664e86db569747e184f05
Mo Yilin <1204992165@qq.com>
2021-03-27 16:44:18 +0800

    Create coursera R programming  homework.R

---
+++ coursera R programming  homework.R
@@ -0,0 +1,38 @@
+library(matlib)
+makeCacheMatrix <- function(x = matrix()) {
+if (ncol(x)==nrow(x) && det(x)!=0) {
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        #setinverse <- function() m <<- inv(x)
+         setinverse <- function() m <- inv(x)
+        getinverse<-function() m
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+
+}else{
+        return(message("The matrix is'n invertible."))
+}
+}
+
+
+cacheSolve <- function(x, ...) {
+        #m<-x$getinverse
+         m<-x$getinverse()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        #data<-x$get
+         data<-x$get()
+        m <- inv(data, ...)
+        x$setinverse(m)
+        m
+}
+
+x <-makeCacheMatrix(matrix(c(1,0,0,0,1,0,0,0,2),ncol=3,nrow=3))
+cacheSolve(x)
+x$get()
+x$getinverse()
